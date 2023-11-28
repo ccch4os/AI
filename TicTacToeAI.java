5e224fecff4a5f22fcd40448683d4fc6b5c140e5
ccch4os <152285508+ccch4os@users.noreply.github.com>
2023-11-28 18:26:57 +0530

    Update TicTacToeAI.java

--- TicTacToeAI.java
+++ TicTacToeAI.java
@@ -1,228 +1,165 @@
-import java.util.*;
+#include <stdio.h>
 
-public class TicTacToeAI {
-    private static final int BOARD_SIZE = 3;
-    private static final char EMPTY_CELL = ' ';
-    private static final char HUMAN_PLAYER = 'X';
-    private static final char AI_PLAYER = 'O';
-
-    private static char[][] board = new char[BOARD_SIZE][BOARD_SIZE];
-
-    public static void main(String[] args) {
-        initializeBoard();
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Tic Tac Toe - Human vs. Computer");
-        System.out.println("You are X, and the Computer is O.");
-        printNumberedBoard();
-
-        while (!isGameOver()) {
-            humanTurn(scanner);
-            printNumberedBoard();
-
-            if (isGameOver()) {
-                break;
-            }
-
-            aiTurn();
-            printNumberedBoard();
+int is_board_full(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == ' ')
+                return 0; 
         }
+    }
+    return 1; 
+}
 
-        scanner.close();
 
-        char winner = checkWinner();
-        if (winner == HUMAN_PLAYER) {
-            System.out.println("Congratulations! You win!");
-        } else if (winner == AI_PLAYER) {
-            System.out.println("Computer wins! Better luck next time.");
+int check_winner(char player, char board[3][3]) {
+
+    for (int i = 0; i < 3; i++) {
+        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player))
+            return 1; 
+    }
+    
+
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player))
+        return 1;
+    
+    return 0; 
+}
+
+int minimax(char board[3][3], int isMaximizing) {
+    if (check_winner('X', board)){
+      return -10;
+    }
+    if (check_winner('O', board)){
+      return 10;
+    }     
+    if (is_board_full(board)){
+      return 0;
+    }
+
+    // Maximizing ai 
+    if (isMaximizing) {
+        int bestScore = -10;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    board[i][j] = 'O';
+                    int score = minimax(board, 0);
+                    board[i][j] = ' '; 
+                    bestScore = (score > bestScore) ? score : bestScore;
+                }
+            }
+        }
+        return bestScore;
+    }
+
+    // Minimizing player
+    else {
+        int bestScore = 10;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    board[i][j] = 'X';
+                    int score = minimax(board, 1);
+                    board[i][j] = ' '; 
+                    bestScore = (score < bestScore) ? score : bestScore;
+                }
+            }
+        }
+        return bestScore;
+    }
+}
+
+void find_best_move(char board[3][3]) {
+    int bestMoveScore = -11;
+    int bestMoveRow = -1;
+    int bestMoveCol = -1;
+    
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == ' ') {
+                board[i][j] = 'O';
+                int moveScore = minimax(board, 0);
+                board[i][j] = ' '; 
+
+                if (moveScore > bestMoveScore) {
+                    bestMoveScore = moveScore;
+                    bestMoveRow = i;
+                    bestMoveCol = j;
+                }
+            }
+        }
+    }
+    
+    printf("Computer's move: %d\n", (bestMoveRow * 3) + bestMoveCol + 1);
+    board[bestMoveRow][bestMoveCol] = 'O';
+}
+
+
+void print_board(char board[3][3]) {
+    printf("-------------\n");
+    for (int i = 0; i < 3; i++) {
+        printf("| %c | %c | %c |\n", board[i][0], board[i][1], board[i][2]);
+        printf("-------------\n");
+    }
+}
+
+int main() {
+    char board[3][3] = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+    
+    int currentPlayer = 0;
+    int moves = 0;
+
+    printf("Tic Tac Toe\n");
+    printf("You are X, and the computer is O. Make your move by entering a number from 1 to 9.\n");
+
+    while (!is_board_full(board)) {
+        print_board(board);
+
+        if (currentPlayer == 0) {
+            int move;
+            printf("Your move: ");
+            scanf("%d", &move);
+
+            int row = (move - 1) / 3;
+            int col = (move - 1) % 3;
+
+            if (move >= 1 && move <= 9 && board[row][col] == ' ') {
+                board[row][col] = 'X';
+                currentPlayer = 1;
+                moves++;
+            } else {
+                printf("Invalid move. Try again.\n");
+            }
         } else {
-            System.out.println("It's a draw! Well played!");
+            find_best_move(board);
+            currentPlayer = 0;
+            moves++;
+        }
+
+      if(moves > 4){
+          if (check_winner('X', board)) {
+            print_board(board);
+            printf("Congratulations! You won!\n");
+            break;
+        } else if (check_winner('O', board)) {
+            print_board(board);
+            printf("The computer won! Better luck next time!\n");
+            break;
         }
     }
 
-    private static void initializeBoard() {
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            for (int j = 0; j < BOARD_SIZE; j++) {
-                board[i][j] = EMPTY_CELL;
-            }
+        if (moves == 9) {
+            print_board(board);
+            printf("It's a draw!\n");
+            break;
         }
     }
 
-    private static void printNumberedBoard() {
-        System.out.println("-------------");
-        int cellNumber = 1;
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            System.out.print("| ");
-            for (int j = 0; j < BOARD_SIZE; j++) {
-                if (board[i][j] == EMPTY_CELL) {
-                    System.out.print(cellNumber);
-                } else {
-                    System.out.print(board[i][j]);
-                }
-                System.out.print(" | ");
-                cellNumber++;
-            }
-            System.out.println();
-            System.out.println("-------------");
-        }
-    }
-
-    private static void humanTurn(Scanner scanner) {
-        System.out.println("Your turn. Enter a cell number (1-9):");
-        int cellNumber = scanner.nextInt();
-
-        if (isValidMove(cellNumber) && board[getRow(cellNumber)][getCol(cellNumber)] == EMPTY_CELL) {
-            board[getRow(cellNumber)][getCol(cellNumber)] = HUMAN_PLAYER;
-        } else {
-            System.out.println("Invalid move. Try again.");
-            humanTurn(scanner);
-        }
-    }
-
-    private static boolean isValidMove(int cellNumber) {
-        return cellNumber >= 1 && cellNumber <= 9;
-    }
-
-    private static int getRow(int cellNumber) {
-        switch (cellNumber) {
-            case 1:
-            case 2:
-            case 3:
-                return 0;
-            case 4:
-            case 5:
-            case 6:
-                return 1;
-            case 7:
-            case 8:
-            case 9:
-                return 2;
-        }
-
-        return 0;
-    }
-
-    private static int getCol(int cellNumber) {
-        switch (cellNumber) {
-            case 1:
-            case 4:
-            case 7:
-                return 0;
-            case 2:
-            case 5:
-            case 8:
-                return 1;
-            case 3:
-            case 6:
-            case 9:
-                return 2;
-        }
-
-        return 0;
-    }
-
-    private static void aiTurn() {
-        int[] bestMove = findBestMove();
-        board[bestMove[0]][bestMove[1]] = AI_PLAYER;
-    }
-
-    private static int[] findBestMove() {
-        int bestScore = Integer.MIN_VALUE;
-        int[] bestMove = new int[]{-1, -1};
-
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            for (int j = 0; j < BOARD_SIZE; j++) {
-                if (board[i][j] == EMPTY_CELL) {
-                    board[i][j] = AI_PLAYER;
-                    int score = minimax(board, false);
-                    board[i][j] = EMPTY_CELL;
-                    if (score > bestScore) {
-                        bestScore = score;
-                        bestMove[0] = i;
-                        bestMove[1] = j;
-                    }
-                }
-            }
-        }
-
-        return bestMove;
-    }
-
-    private static int minimax(char[][] board, boolean isMaximizingPlayer) {
-        if (isGameOver()) {
-            return evaluateBoard();
-        }
-
-        if (isMaximizingPlayer) {
-            int bestScore = Integer.MIN_VALUE;
-            for (int i = 0; i < BOARD_SIZE; i++) {
-                for (int j = 0; j < BOARD_SIZE; j++) {
-                    if (board[i][j] == EMPTY_CELL) {
-                        board[i][j] = AI_PLAYER;
-                        bestScore = Math.max(bestScore, minimax(board, false)); // AI
-                        board[i][j] = EMPTY_CELL;
-                    }
-                }
-            }
-            return bestScore;
-        } else {
-            int bestScore = Integer.MAX_VALUE;
-            for (int i = 0; i < BOARD_SIZE; i++) {
-                for (int j = 0; j < BOARD_SIZE; j++) {
-                    if (board[i][j] == EMPTY_CELL) {
-                        board[i][j] = HUMAN_PLAYER;
-                        bestScore = Math.min(bestScore, minimax(board, true)); // Player
-                        board[i][j] = EMPTY_CELL;
-                    }
-                }
-            }
-            return bestScore;
-        }
-    }
-
-    private static boolean isGameOver() {
-        return isBoardFull() || checkWinner() != EMPTY_CELL;
-    }
-
-    private static boolean isBoardFull() {
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            for (int j = 0; j < BOARD_SIZE; j++) {
-                if (board[i][j] == EMPTY_CELL) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    private static char checkWinner() {
-        for (int i = 0; i < BOARD_SIZE; i++) {
-            if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != EMPTY_CELL) {
-                return board[i][0];
-            }
-            if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != EMPTY_CELL) {
-                return board[0][i];
-            }
-        }
-        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != EMPTY_CELL) {
-            return board[0][0];
-        }
-        if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != EMPTY_CELL) {
-            return board[0][2];
-        }
-
-        return EMPTY_CELL;
-    }
-
-    private static int evaluateBoard() {
-        char winner = checkWinner();
-        if (winner == AI_PLAYER) {
-            return 10;
-        } else if (winner == HUMAN_PLAYER) {
-            return -10;
-        } else {
-            return 0;
-        }
-    }
+    return 0;
 }
